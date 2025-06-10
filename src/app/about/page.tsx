e9f9affa537269a9481372003d8cf1f47870bcbc
dexter-nym <draftworks000@gmail.com>
2025-06-11 03:44:37 +0530

    feat: Add footer to about page and implement card pinning in VisionMission

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,20 @@
+"use client";
+
 import React from "react";
 import DemoHero from "../Components/DemoHero/DemoHero";
+import VisionMission from "../Components/VisionMission/VisionMission";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
-export default function AboutPage() {
+const AboutPage = () => {
   return (
     <main>
       <Navbar/>
       <DemoHero />
+      <VisionMission />
+      <Footer />
     </main>
   );
-}
+};
+
+export default AboutPage;
