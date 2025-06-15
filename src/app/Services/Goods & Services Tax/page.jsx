4ddabdbc434067943bf42a741bec4134cbd5d0fd
dexter-nym <draftworks000@gmail.com>
2025-06-15 11:34:43 +0530

    marquee added and height corrrected to 14vh

---
+++ src/app/Services/Goods & Services Tax/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Goods & Services Tax Page</h1>
+    </div>
+  );
+};
+
+export default Page; 
---
+++ src/app/Services/Income Tax/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Income Tax Page</h1>
+    </div>
+  );
+};
+
+export default Page; 
